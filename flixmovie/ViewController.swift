b5f68c16d66e79399ebd3d7fcc9757df2a01fdd1
duytran <duytran>
2020-02-20 23:06:44 -0800

    Initial Commit

---
+++ flixmovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixmovie
+//
+//  Created by Duy Tran Vinh Thai on 2/20/20.
+//  Copyright © 2020 Duy Tran Vinh Thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
